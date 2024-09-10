b0759ee64083f50de665509a232c53ba22e4e4e6
Eloups <121400663+Eloups@users.noreply.github.com>
2024-09-10 16:19:43 +0200

    Privilégiez la méthode post

--- submit_contact.php
+++ submit_contact.php
@@ -20,12 +20,12 @@
         <h1>Message bien reçu !</h1>
         <div class="card">
             <div class="card-body">
-                <?php if((!isset($_GET['email']) || !filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) || (!isset($_GET['message']) || empty($_GET['message']))){ ?>
+                <?php if((!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) || (!isset($_POST['message']) || empty($_POST['message']))){ ?>
                     <h5 class="card-title">Il faut un email et un message valides pour soumettre le formulaire</h5>
                 <?php } else { ?>
                     <h5 class="card-title">Rappel de vos informations</h5>
-                    <p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?> </p>
-                    <p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?> </p>
+                    <p class="card-text"><b>Email</b> : <?php echo $_POST['email']; ?> </p>
+                    <p class="card-text"><b>Message</b> : <?php echo $_POST['message']; ?> </p>
                 <?php } ?>
             </div>
         </div>
